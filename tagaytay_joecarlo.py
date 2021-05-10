1a36394afcf57db3511783d72f5728bc24192247
joecarlo <80028279+joecarlo@users.noreply.github.com>
2021-05-10 21:58:30 +0800

    Update tagaytay_joecarlo.py

--- tagaytay_joecarlo.py
+++ tagaytay_joecarlo.py
@@ -9,7 +9,8 @@ class student:
         self.root = root
         blank_space = " "
         self.root.title(200 * blank_space + "Student Information System")
-        self.root.geometry("1353x445+0+0")
+        self.root.geometry("1150x475+0+0")
+        self.root.config(bg="yellow")
         self.data = dict()
         self.temp = dict()
         self.filename = 'csc.csv'
@@ -45,7 +46,7 @@ class student:
                                                    'Year Level': YearLevel.get()}
                     self.saveData()
                     tkinter.messagebox.showinfo("SIS", "Student Recorded Successfully")
-                Clear()
+                #Clear()
                 displayStudent()
 
         def displayStudent():
@@ -114,104 +115,89 @@ class student:
                     self.saveData()
                     tkinter.messagebox.showinfo("SIS", "Student Recorded Successfully")
                 Clear()
-                displayStudent()     
-            
+                displayStudent()   
         #==============================frame
         
-        MainFrame = Frame(self.root, bd=10, width=1000, height=900, relief=RIDGE, bg="gray")
-        MainFrame.grid()
+        TitleFrame = Frame(self.root, bd=5, width=1000, bg="gray10",height=90, relief=RIDGE)
+        TitleFrame.pack(side=TOP)
         
-        TitleFrame = Frame(MainFrame, bd=5, width=1000, height=100, bg="gray")
-        TitleFrame.grid(row = 0, column=0)
-        
-        TopFrame1 = Frame(MainFrame, bd=5, width=1000, height=50, bg="yellow")
-        TopFrame1.grid(row = 1, column=0)
-        
-        TopFrame2 = Frame(MainFrame, bd=5, width=1000, height=450)
-        TopFrame2.grid(row = 2, column=0)
-        
-        LeftFrame = Frame(TopFrame2, bd=5, width=1000, height=400, padx=2, bg="black")
-        LeftFrame.pack(side=LEFT)
-        
-        LeftFrame1 = Frame(LeftFrame, bd=5, width=600, height=180, padx=2, pady=4)
-        LeftFrame1.pack(side=TOP,padx=0,pady=0)
-        
-        RightFrame1 = Frame(TopFrame2, bd=5, width=400, height=400, padx=2, bg="light gray")
-        RightFrame1.pack(side=RIGHT)
+        LeftFrame1 = Frame(self.root,bg="yellow", bd=5, width=600, height=250, padx=2, pady=4)
+        LeftFrame1.pack(side=LEFT,padx=0,pady=0)
         
         #============================title
         
-        self.lblTitle = Label(TitleFrame, font=('arial', 59 , 'bold'), text="Student Information System", bd=7)
+        self.lblTitle = Label(TitleFrame, font=('arial', 40 , 'bold'), text="STUDENT INFORMATION SYSTEM",bg="gray10",fg="snow",bd=7)
         self.lblTitle.grid(row=0, column=0, padx=132)
         
-        #========================labels and entry
+         #========================labels and entry
         
-        self.lblStudentID = Label(LeftFrame1, font=('arial', 10, 'bold'), text="Student ID Number", bd=7, anchor=W)
+        self.lblStudentID = Label(LeftFrame1, font=('arial', 13, 'bold'), bg="yellow",text="Student ID Number", bd=7, anchor=W)
         self.lblStudentID.grid(row=0, column=0)
-        self.lblStudentID = Label(LeftFrame1, font=('arial', 8), text="yyyy-nnnn", bd=7)
+        self.lblStudentID = Label(LeftFrame1, font=('arial', 10), bg="yellow", text="yyyy-nnnn", bd=7, anchor=W)
         self.lblStudentID.grid(row=1, column=1)
-        self.lblStudentID = Entry(LeftFrame1, font=('arial', 10, 'bold'), bd=7, width=40, justify='left', textvariable = StudentIDNumber)
+        self.lblStudentID = Entry(LeftFrame1, font=('arial', 10, 'bold'), bd=7, width=48, justify='left', textvariable = StudentIDNumber)
         self.lblStudentID.grid(row=0, column=1)
         
-        self.lblFullName = Label(LeftFrame1, font=('arial', 12, 'bold'), text="Full Name", bd=7, anchor=W)
+        self.lblFullName = Label(LeftFrame1, font=('arial', 13, 'bold'),bg="yellow", text="Full Name", bd=7, anchor=W)
         self.lblFullName.grid(row=2, column=0)
-        self.lblFullName = Label(LeftFrame1, font=('arial', 8), text="Lastname, Firstname Middleinitial", bd=7)
+        self.lblFullName = Label(LeftFrame1, font=('arial', 10),bg="yellow", text="Lastname, Firstname Middleinitial", bd=7, anchor=W)
         self.lblFullName.grid(row=3, column=1)
-        self.lblFullName = Entry(LeftFrame1, font=('arial', 12, 'bold'), bd=7, width=30, justify='left', textvariable = StudentName)
+        self.lblFullName = Entry(LeftFrame1, font=('arial', 10, 'bold'), bd=7, width=48, textvariable = StudentName)
         self.lblFullName.grid(row=2, column=1)
         
-        self.lblCourse = Label(LeftFrame1, font=('arial', 10, 'bold'), text="Course", bd=7, anchor=W)
+        self.lblCourse = Label(LeftFrame1, font=('arial', 13, 'bold'),bg="yellow", text="Course", bd=7, anchor=W)
         self.lblCourse.grid(row=4, column=0)
-        self.lblCourse = Entry(LeftFrame1, font=('arial', 10, 'bold'), bd=7, width=40, justify='left', textvariable = Course)
+        self.lblCourse = Entry(LeftFrame1, font=('arial', 10, 'bold'), bd=7, width=48, justify='left', textvariable = Course)
         self.lblCourse.grid(row=4, column=1)
         
-        self.lblYearLevel = Label(LeftFrame1, font=('arial', 10, 'bold'), text="Year Level", bd=7, anchor=W)
+        self.lblYearLevel = Label(LeftFrame1, font=('arial', 13, 'bold'), bg="yellow",text="Year Level", bd=7, anchor=W)
         self.lblYearLevel.grid(row=5, column=0)
-        self.lblYearLevel= ttk.Combobox(LeftFrame1, font=('arial', 10, 'bold'), state='readonly', width=39, textvariable = YearLevel)
+        self.lblYearLevel= ttk.Combobox(LeftFrame1, font=('arial', 10, 'bold'), state='readonly', width=47, textvariable = YearLevel)
         self.lblYearLevel['values'] = ('','1st Year','2nd Year','3rd Year','4th Year','5th Year')
         self.lblYearLevel.current(0)
         self.lblYearLevel.grid(row=5, column=1)
         
-        self.lblGender = Label(LeftFrame1, font=('arial', 10, 'bold'), text="Gender", bd=7, anchor=W)
+        self.lblGender = Label(LeftFrame1, font=('arial', 13, 'bold'),bg="yellow", text="Gender", bd=7, anchor=W)
         self.lblGender.grid(row=6, column=0)
-        self.lblGender = ttk.Combobox(LeftFrame1, font=('arial', 10, 'bold'), state='readonly', width=39, textvariable = Gender)
+        self.lblGender = ttk.Combobox(LeftFrame1, font=('arial', 10, 'bold'), state='readonly', width=47, textvariable = Gender)
         self.lblGender['values'] = ('','Female','Male')
         self.lblGender.current(0)
         self.lblGender.grid(row=6, column=1)
         
-        self.Search = Entry(TopFrame1, font=("Poppins", 10), textvariable=Search, width=23)
-        self.Search.grid(row=0, column=8)
+        self.Search = Entry(self.root, font=("Poppins", 10), textvariable=Search, width=19)
+        self.Search.place(x=560,y=100)
         self.Search.insert(0,'Search ID Number')
-        
         #=====================buttons
         
-        self.btnAddNew=Button(TopFrame1, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='ADD', command = addData)
-        self.btnAddNew.grid(row=0, column=0)
+        self.btnAddNew=Button(self.root, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='ADD', command = addData)
+        self.btnAddNew.place(x=15,y=410)
 
-        self.btnUpdate=Button(TopFrame1, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='UPDATE', command = updateStudent)
-        self.btnUpdate.grid(row=0, column=1)
+        self.btnUpdate=Button(self.root, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='UPDATE', command = updateStudent)
+        self.btnUpdate.place(x=150,y=410)
 
-        self.btnSelect=Button(TopFrame1, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=11, text='SELECT',command = editStudent)
-        self.btnSelect.grid(row=0, column=3)
+        self.btnDelete=Button(self.root, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='DELETE',command = deleteData)
+        self.btnDelete.place(x=285,y=410)
 
-        self.btnDelete=Button(TopFrame1, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='DELETE',command = deleteData)
-        self.btnDelete.grid(row=0, column=4)
-
-        self.btnSearch=Button(TopFrame1, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='SEARCH', command = searchStudent)
-        self.btnSearch.grid(row=0, column=7)
-
-        self.btnClear=Button(TopFrame1, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='CLEAR', command=Clear)
-        self.btnClear.grid(row=0, column=6)
+        self.btnClear=Button(self.root, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='CLEAR', command=Clear)
+        self.btnClear.place(x=420,y=410)
         
-        self.btnRefresh=Button(TopFrame1, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='DISPLAY', command=displayStudent)
-        self.btnRefresh.grid(row=0, column=5)
+        self.btnSearch=Button(self.root, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='SEARCH', command = searchStudent)
+        self.btnSearch.place(x=710,y=95)
         
+        self.btnRefresh=Button(self.root, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='SHOW ALL', command=displayStudent)
+        self.btnRefresh.place(x=855,y=95)
+        
+        self.btnSelect=Button(self.root, pady=1,bd=4,font=('arial',10,'bold'), padx=24, width=8, text='SELECT',command = editStudent)
+        self.btnSelect.place(x=1000,y=95)
+                
         #======================treeview
         
-        scroll_y=Scrollbar(RightFrame1, orient=VERTICAL)
-        tree = ttk.Treeview(RightFrame1,columns=("ID Number", "Name", "Gender", "Course", "Year Level"),
+        scroll_y=Scrollbar(self.root, orient=VERTICAL)
+        tree = ttk.Treeview(self.root,height = 15,columns=("ID Number", "Name", "Gender", "Course", "Year Level"),
                                         yscrollcommand=scroll_y.set)
         scroll_y.pack(side=RIGHT, fill=Y)
+        scroll_y.place(x=1130,y=128,height=330)
+    
         tree.heading("ID Number", text="ID Number")
         tree.heading("Name", text="Name")
         tree.heading("Course", text="Course")
@@ -223,11 +209,12 @@ class student:
         tree.column("Course", width=99)
         tree.column("Year Level", width=99)
         tree.column("Gender", width=93)
+        scroll_y.config(command=tree.yview)
         tree.pack(fill=BOTH,expand=1)
+        tree.place(x=560,y=130)
         
         displayStudent()
-        
-        
+                
     def saveData(self):
         datalist = []
         with open(self.filename, "w", newline='') as u:
